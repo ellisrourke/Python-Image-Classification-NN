5e6264711275c3e0834e21e4d10ae57bd24438cd
Ellis Rourke <50188668+ellisrourke@users.noreply.github.com>
2020-05-14 20:38:02 +1000

    Update dataLoader.py

--- dataLoader.py
+++ dataLoader.py
@@ -1,4 +1,15 @@
 import numpy as np
 import gzip
 
-def loadMnistData(trainData,trainLabel,testData,testLabel)
+def load_mnnist(train_x, train_y, test_x, test_y):
+    testX = np.loadtxt(gzip.open(test_x, "rb"), delimiter=",")
+    testY = [parse(y) for y in np.loadtxt(gzip.open(test_y, "rb"))]
+    trainingX = np.loadtxt(gzip.open(train_x, "rb"), delimiter=",")
+    trainingY = [parse(y) for y in np.loadtxt(gzip.open(train_y, "rb"))]
+    return trainingX, trainingY, testX, testY
+
+
+def parse(val):
+    data = np.zeros(10)
+    data[int(val)] = 1.0
+    return data
